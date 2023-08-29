f60e34b50b2979f95215e0285d3289dec2080607
David J. Gates <44448505+geoguygates@users.noreply.github.com>
2023-08-28 22:49:44 -0500

    Update quilt.py

--- src/quilt.py
+++ src/quilt.py
@@ -1,30 +1,34 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
-# Create a 3 x 4 grid of subplots
-fig, axs = plt.subplots(3, 4, figsize=(12, 9))
+# Define the figure and a 3x4 grid of subplots
+fig, axs = plt.subplots(4, 3, figsize=(12, 9))
 
-# Iterate over each subplot
-for i in range(3):
-    for j in range(4):
-        ax = axs[i, j]
-        
-        # Generate concentric squares representing days of a month
-        num_squares = np.random.choice([30, 31])  # Randomly select 30 or 31 squares
-        square_sizes = np.arange(1, num_squares + 1)[::-1]
-        
-        # Plot concentric squares
-        ax.pie(square_sizes, colors='white', startangle=90, counterclock=False, wedgeprops={'linewidth': 1, 'edgecolor': 'black'})
-        
-        # Set aspect ratio to equal for a square plot
-        ax.set_aspect('equal')
-        
-        # Set title as the month
-        month = f"Month {i*4 + j + 1}"
-        ax.set_title(month)
-        
-# Adjust spacing between subplots
-plt.subplots_adjust(hspace=0.5)
+# Center of each set of concentric squares within its subplot
+center = (0.5, 0.5)
 
-# Show the plot
+# Generate 30 sizes using linspace from 0.02 to 1.0 (small to large)
+sizes = np.linspace(1.0, 0.02, 30)
+
+# Use a colormap to get 30 colors
+colormap = plt.cm.viridis
+colors = [colormap(i) for i in np.linspace(0, 1, 30)]
+
+# Function to draw concentric squares on a given axis
+def draw_squares(ax):
+    for size, color in zip(sizes, colors):
+        lower_left_corner = (center[0] - size / 2, center[1] - size / 2)
+        rect = patches.Rectangle(lower_left_corner, size, size, fill=True, color=color)
+        ax.add_patch(rect)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_aspect('equal', 'box')
+    ax.axis('off')  # Hide axis
+
+# Iterate over each subplot and draw concentric squares
+for ax in axs.ravel():
+    draw_squares(ax)
+
+plt.tight_layout()
 plt.show()
